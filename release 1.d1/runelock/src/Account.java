7757dc50b134336eea8ae8469a454a4027bcd43a
Christian <96608792+krispkreme@users.noreply.github.com>
2026-03-29 22:29:31 -0600

    Add getters and JSON property annotation to Account

--- release 1.d1/runelock/src/Account.java
+++ release 1.d1/runelock/src/Account.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 import java.util.ArrayList;
+import com.fasterxml.jackson.annotation.JsonProperty;
 /**
  *
  * @author ckurd
@@ -13,7 +14,33 @@ public class Account {
     String username;
     String password;
     ArrayList tags;
+    @JsonProperty("created")
     String createdDate;
     String lastUsed;
+    
+    
+    public String getURL(){
+        return this.url;
+    }
+    
+    public String getUsername(){
+        return this.username;
+    }
+    
+    public String getPassword(){
+        return this.password;
+    }
+    
+    public ArrayList getTags(){
+        return this.tags;
+    }
+    
+    public String getCreatedDate(){
+        return this.createdDate;
+    }
+    
+    public String getLastUsed(){
+        return this.lastUsed;
+    }
     
 }
